5a450c219a0f98155f89ea561cad87183dcaa439
Bogdan00004 <bogdankralj004@gmail.com>
2025-11-27 16:24:41 +0100

    Napravljena klasa Parfem

---
+++ Domain/Modeli/Parfem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Modeli
+{
+    public class Parfem
+    {
+        public Guid Id { get; set; }
+        public string Naziv {  get; set; }
+        public string Tip {  get; set; }
+        public int NetoKolicina {  get; set; }
+        public string SerijskiBroj {  get; set; }
+        public Guid BiljkaId {  get; set; }
+        public DateTime RokTrajanja { get; set; }
+    }
+}
