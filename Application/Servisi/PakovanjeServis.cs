b50f7f153e57babb70dca92ebacf3379cc3fc958
Bogdan00004 <bogdankralj004@gmail.com>
2025-11-29 23:58:58 +0100

    Implementiran PakovanjeServis sa funkcionalnostima za pakovanje parfema u ambalažu i slanje ambalaže u skladište

--- Application/Servisi/PakovanjeServis.cs
+++ Application/Servisi/PakovanjeServis.cs
@@ -11,9 +11,75 @@ using System.Threading.Tasks;
 
 namespace Loger_Bloger.Servisi
 {
-    public class PakovanjeServis 
+    public class PakovanjeServis : IPakovanjeServis
     {
-        
-        
+        private readonly IParfemRepozitorijum _parfemiRepozitorijum;
+        private readonly IAmbalazaRepozitorijum _ambalazaRepozitorijum;
+        private readonly ISkladistaRepozitorijum _skladistaRepozitorijum;
+        private readonly IPreradaServis _preradaServis;
+
+
+        public PakovanjeServis(
+        IParfemRepozitorijum parfemiRepozitorijum,
+        IAmbalazaRepozitorijum ambalazaRepozitorijum,
+        ISkladistaRepozitorijum skladistaRepozitorijum,
+        IPreradaServis preradaServis)
+        {
+            _parfemiRepozitorijum = parfemiRepozitorijum;
+            _ambalazaRepozitorijum = ambalazaRepozitorijum;
+            _skladistaRepozitorijum = skladistaRepozitorijum;
+            _preradaServis = preradaServis;
+        }
+
+        public void SpakujParfeme(string nazivParfema, int brojBocica, double zapreminaPoBocici, Guid skladisteId)
+        {
+            var parfemi = _preradaServis.PreradiBiljke(nazivParfema, brojBocica, zapreminaPoBocici);
+
+            foreach (var parfem in parfemi)
+            {
+                _parfemiRepozitorijum.Dodaj(parfem);
+            }
+
+            var ambalaza = new Ambalaza
+            {
+                SkladisteId = skladisteId
+            };
+
+            foreach (var parfem in parfemi)
+            {
+                ambalaza.ParfemiId.Add(parfem.Id);
+            }
+
+            _ambalazaRepozitorijum.Dodaj(ambalaza);
+            _ambalazaRepozitorijum.SacuvajPromene();
+        }
+
+        public void PosaljiAmbalazuUSkladiste(Guid skladisteId)
+        {
+            var skladiste = _skladistaRepozitorijum.NadjiPoId(skladisteId);
+            var dostupneAmbalaze = _ambalazaRepozitorijum.VratiSpakovaneAmbalaze();
+
+
+            if (skladiste == null)
+                throw new Exception("Skladiste ne postoji.");
+
+
+            if (skladiste.TrenutniKapacitet >= skladiste.MaxKapacitet)
+                throw new Exception("Skladiste je popunjeno.");
+
+
+            var ambalaza = dostupneAmbalaze.FirstOrDefault(a => a.Status == StatusAmbalaze.Spakovana);
+
+            if (ambalaza == null)
+                throw new Exception("Nema dostupnih spakovanih ambalaza.");
+
+
+            ambalaza.Status = StatusAmbalaze.Poslata;
+            skladiste.TrenutniKapacitet++;
+
+
+            _ambalazaRepozitorijum.SacuvajPromene();
+            _skladistaRepozitorijum.SacuvajPromene();
+        }
     }
 }
---
+++ Domain/Servisi/IPreradaServis.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Modeli;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Servisi
+{
+    public interface IPreradaServis
+    {
+        List<Parfem> PreradiBiljke(string naziv, int brojBocica, double zapreminaPoBocici);
+    }
+}
