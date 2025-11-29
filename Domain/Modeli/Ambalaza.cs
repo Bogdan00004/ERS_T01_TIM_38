ca70d1bc511931316c75a8676c2d4888671875a9
Bogdan00004 <bogdankralj004@gmail.com>
2025-11-29 21:57:38 +0100

    Napravljena klasa Ambalaza

---
+++ Domain/Modeli/Ambalaza.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Enumeracije;
+
+namespace Domain.Modeli
+{
+    public class Ambalaza
+    {
+        public Guid Id { get; set; }
+        public string Naziv { get; set; }
+        public string AdresaPosiljaoca { get; set; }
+        public Guid SkladisteId { get; set; }
+        public List<Guid>ParfemiId { get; set; }
+        public StatusAmbalaze Status {  get; set; }
+        
+        public Ambalaza()
+        {
+            Id = Guid.NewGuid();
+            ParfemiId = new List<Guid>();
+            Status = StatusAmbalaze.Spakovana;
+        }
+    }
+}
