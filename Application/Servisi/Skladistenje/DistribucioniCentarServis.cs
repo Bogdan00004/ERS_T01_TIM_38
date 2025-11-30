f80643b205f30758a526c01363446759712d1456
Bogdan00004 <bogdankralj004@gmail.com>
2025-11-30 15:36:56 +0100

    Implementiran interfejs ISkladistenjeServis i distribucioni centar servis sa asihronom logikom slanja ambalaža

---
+++ Application/Servisi/Skladistenje/DistribucioniCentarServis.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Modeli;
+using Domain.Repozitorijumi;
+using Domain.Servisi;
+using Domain.Enumeracije;
+
+namespace Loger_Bloger.Servisi.Skladistenje
+{
+    public class DistribucioniCentarServis:ISkladistenjeServis
+    {
+        private readonly ISkladistaRepozitorijum _skladistaRepozitorijum;
+        private readonly IAmbalazaRepozitorijum _ambalazaRepozitorijum;
+
+        public DistribucioniCentarServis(
+            ISkladistaRepozitorijum skladistaRepozitorijum,
+            IAmbalazaRepozitorijum ambalazaRepozitorijum)
+        {
+            _skladistaRepozitorijum = skladistaRepozitorijum;
+            _ambalazaRepozitorijum = ambalazaRepozitorijum;
+        }
+
+        public async Task<List<Ambalaza>> PosaljiAmbalazeProdaji(int brojZaSlanje)
+        {
+            var sveAmbalaze = _ambalazaRepozitorijum.VratiSve();
+            var poslateAmbalaze = new List<Ambalaza>();
+
+            // Maksimalno 3 ambalaže po slanju
+            int brojZaistaZaSlanje = Math.Min(brojZaSlanje, 3);
+
+            var dostupne = sveAmbalaze.Where(a => a.Status == StatusAmbalaze.Spakovana).Take(brojZaistaZaSlanje).ToList();
+
+            foreach (var ambalaza in dostupne)
+            {
+                await Task.Delay(500); // 0.5 sekundi po ambalaži
+                ambalaza.Status = StatusAmbalaze.Poslata;
+                poslateAmbalaze.Add(ambalaza);
+            }
+
+            _ambalazaRepozitorijum.SacuvajPromene();
+            return poslateAmbalaze;
+        }
+    }
+}
---
+++ Domain/Servisi/ISkladistenjeServis.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Modeli;
+
+namespace Domain.Servisi
+{
+    public interface ISkladistenjeServis
+    {
+        Task<List<Ambalaza>> PosaljiAmbalazeProdaji(int brojZaSlanje);
+    }
+}
