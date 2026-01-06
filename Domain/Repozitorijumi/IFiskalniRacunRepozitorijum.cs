f1b5e227c94fd4cbec01f3a0e3ceda404669ec3b
Nidzovantije <Nidzovantije@DESKTOP-N6QPOS7>
2026-01-06 22:19:46 +0100

    fiskalni racun interfejs

---
+++ Domain/Repozitorijumi/IFiskalniRacunRepozitorijum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Domain.Modeli;
+
+namespace Domain.Repozitorijumi;
+
+public interface IFiskalniRacunRepozitorijum
+{
+    void Dodaj(FiskalniRacun racun);
+    List<FiskalniRacun> VratiSve();
+    void SacuvajPromene();
+}
