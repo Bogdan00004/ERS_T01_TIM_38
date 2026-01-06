9f0c757110478458e0f60385ff9cc6e6292bf507
Nidzovantije <Nidzovantije@DESKTOP-N6QPOS7>
2026-01-06 23:46:00 +0100

    opcije meni

--- Application/Program.cs
+++ Application/Program.cs
@@ -6,11 +6,8 @@ using Domain.Modeli;
 using Domain.Repozitorijumi;
 using Domain.Servisi;
 using Loger_Bloger.Servisi.Skladistenje;
-using Presentation.Authentifikacija;
 using Presentation.Meni;
 using Services.AutentifikacioniServisi;
-using Services.Implementacije;
-using Services.Interfaces;
 
 namespace Loger_Bloger
 {
@@ -19,28 +16,27 @@ namespace Loger_Bloger
         public static void Main()
         {
             // Baza podataka
-            IBazaPodataka bazaPodataka = new JsonBazaPodataka(); 
+            IBazaPodataka bazaPodataka = new JsonBazaPodataka();
 
             // Repozitorijumi
             IKorisniciRepozitorijum korisniciRepozitorijum = new KorisniciRepozitorijum(bazaPodataka);
             ISkladistaRepozitorijum skladistaRepozitorijum = new SkladistaRepozitorijum(bazaPodataka);
             IAmbalazaRepozitorijum ambalazaRepozitorijum = new AmbalazeRepozitorijum(bazaPodataka);
 
+            // DODATO: parfemi + racuni
+            IParfemRepozitorijum parfemRepozitorijum = new ParfemiRepozitorijum(bazaPodataka);
+            IFiskalniRacunRepozitorijum fiskalniRacunRepozitorijum = new FiskalniRacuniRepozitorijum(bazaPodataka);
+
             // Servisi
-            IAutentifikacijaServis autentifikacijaServis = new AutentifikacioniServis(korisniciRepozitorijum); // TODO: Pass necessary dependencies
-            // TODO: Add other necessary services
+            IAutentifikacijaServis autentifikacijaServis = new AutentifikacioniServis(korisniciRepozitorijum);
 
             var magacinskiServis = new MagacinskiCentarServis(skladistaRepozitorijum, ambalazaRepozitorijum);
             var distribucioniServis = new DistribucioniCentarServis(skladistaRepozitorijum, ambalazaRepozitorijum);
-
             ISkladistenjeServisUloge uloga = new SkladistenjeServisUloge(magacinskiServis, distribucioniServis);
 
-            
-
-            // Ako nema nijednog korisnika u sistemu, dodati dva nova
+            // Seed korisnika
             if (korisniciRepozitorijum.SviKorisnici().Count() == 0)
             {
-                // TODO: Add initial users to the system
                 korisniciRepozitorijum.DodajKorisnika(new Korisnik
                 {
                     KorisnickoIme = "Bogdan",
@@ -56,23 +52,33 @@ namespace Loger_Bloger
                     ImePrezime = "Petar Petrovic",
                     Uloga = TipKorisnika.Prodavac
                 });
-
             }
 
-            // Prezentacioni sloj
-            Presentation.Authentifikacija.AutentifikacioniMeni am = new Presentation.Authentifikacija.AutentifikacioniMeni(autentifikacijaServis);
-            Korisnik prijavljen = new Korisnik(); 
+            // Login
+            var am = new Presentation.Authentifikacija.AutentifikacioniMeni(autentifikacijaServis);
+            Korisnik prijavljen = new Korisnik();
+
             while (am.TryLogin(out prijavljen) == false)
             {
                 Console.WriteLine("Pogrešno korisničko ime ili lozinka. Pokušajte ponovo.");
             }
+
+            // Kreiraj skladistenje servis po ulozi
             ISkladistenjeServis skladistenjeServis = uloga.KreirajServis(prijavljen.Uloga);
+
+            
+            IProdajaServis prodajaServis = new Loger_Bloger.Servisi.Prodaja.ProdajaServis(
+                parfemRepozitorijum,
+                ambalazaRepozitorijum,
+                fiskalniRacunRepozitorijum,
+                skladistenjeServis);
+
             Console.Clear();
             Console.WriteLine($"Uspešno ste prijavljeni kao: {prijavljen.ImePrezime} ({prijavljen.Uloga})");
 
-            OpcijeMeni meni = new OpcijeMeni(skladistenjeServis); // TODO: Pass necessary dependencies
+            
+            OpcijeMeni meni = new OpcijeMeni(prijavljen, prodajaServis);
             meni.PrikaziMeni();
-
         }
     }
 }
--- Presentation/Meni/OpcijeMeni.cs
+++ Presentation/Meni/OpcijeMeni.cs
@@ -1,13 +1,18 @@
-﻿using Domain.Servisi;
+﻿using Domain.Modeli;
+using Domain.Servisi;
+using Domain.Enumeracije;
+
 namespace Presentation.Meni
 {
     public class OpcijeMeni
     {
-        private readonly ISkladistenjeServis _skladistenjeServis;
+        private readonly Korisnik _ulogovanKorisnik;
+        private readonly IProdajaServis _prodajaServis;
 
-        public OpcijeMeni(ISkladistenjeServis skladistenjeServis)
+        public OpcijeMeni(Korisnik ulogovanKorisnik, IProdajaServis prodajaServis)
         {
-            _skladistenjeServis = skladistenjeServis;
+            _ulogovanKorisnik = ulogovanKorisnik;
+            _prodajaServis = prodajaServis;
         }
 
         public void PrikaziMeni()
@@ -17,9 +22,68 @@ namespace Presentation.Meni
             bool kraj = false;
             while (!kraj)
             {
-                // TODO: Prikaz opcija menija
+                Console.WriteLine("\n1) Prodaja");
+
+                
+                if (_ulogovanKorisnik.Uloga == TipKorisnika.MenadzerProdaje)
+                    Console.WriteLine("2) Pregled svih fiskalnih računa");
+
+                Console.WriteLine("0) Izlaz");
+                Console.Write("Izbor: ");
+
+                var izbor = Console.ReadLine();
+
+                switch (izbor)
+                {
+                    case "1":
+                        Console.WriteLine("Prodaja - TODO (ERS-31/32/33)");
+                        break;
+
+                    case "2":
+                        if (_ulogovanKorisnik.Uloga == TipKorisnika.MenadzerProdaje)
+                            PrikaziSveRacune();
+                        else
+                            Console.WriteLine("Nemate prava pristupa ovoj opciji.");
+                        break;
+
+                    case "0":
+                        kraj = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Pogrešan izbor.");
+                        break;
+                }
+            }
+        }
+
+        private void PrikaziSveRacune()
+        {
+            var racuni = _prodajaServis.VratiSveRacune();
+
+            Console.WriteLine("\n==================== SVI FISKALNI RACUNI ====================");
+
+            if (racuni == null || racuni.Count == 0)
+            {
+                Console.WriteLine("Nema fiskalnih računa.");
+                return;
+            }
+
+            foreach (var r in racuni)
+            {
+                Console.WriteLine($"ID: {r.Id}");
+                Console.WriteLine($"Tip prodaje: {r.TipProdaje}");
+                Console.WriteLine($"Način plaćanja: {r.NacinPlacanja}");
+                Console.WriteLine($"Iznos: {r.IznosZaNaplatu}");
+
+                Console.WriteLine("Stavke:");
+                foreach (var s in r.Stavke)
+                {
+                    Console.WriteLine($" - {s.NazivParfema} | kom: {s.Kolicina} | cena: {s.CenaPoKomadu} | ukupno: {s.Ukupno}");
+                }
+
+                Console.WriteLine("--------------------------------------------------------------");
             }
         }
     }
-
 }
