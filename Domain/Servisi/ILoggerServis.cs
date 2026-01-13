037b3c411bb18651f8c724fa1005271dec729bcd
Aleksandar Petrović <aco.petrovic.rss@gmail.com>
2026-01-13 17:44:42 +0100

    ILogger (Definisanje metoda za LogInfo,LogWarning,LogError)

--- Domain/Servisi/ILoggerServis.cs
+++ Domain/Servisi/ILoggerServis.cs
@@ -4,6 +4,9 @@ namespace Domain.Servisi
 {
     public interface ILoggerServis
     {
+        bool LogInfo(string poruka);
+        bool LogWarning(string poruka);
+        bool LogError(string poruka);
         public bool EvidentirajDogadjaj(TipEvidencije tip, string poruka);
     }
 }
